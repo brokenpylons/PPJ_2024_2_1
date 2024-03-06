e7316e23ad6af27af53a053a28509b317c2524d0
Žiga Leber <mail+github@zigaleber.com>
2024-03-06 13:36:35 +0100

    Add examples

--- test.sh
+++ test.sh
@@ -1,8 +1,29 @@
+#!/bin/bash
 pwd
-output=$(./run.sh <(cat - <<<"INPUT") >(cat -) >/dev/null)
-cat - <<<"$output"
 
-if [[ "$output" != "ANSWERINPUT" ]]
-then
-    exit 1
-fi
+newline() {
+    echo -e "\n"
+}
+
+declare -r path=$(dirname $(realpath "$0"))
+
+declare -i err=0
+
+for file in "$path"/examples/example{0..50}.txt
+do
+    echo "$file"
+    cat "$file"
+    declare output=$(./run.sh "$file" >(cat -) >/dev/null)
+    declare expected="${file%.*}.expected.txt"
+
+    if [[ "$output" == $(cat "$expected") ]]
+    then
+        echo "[OK]"
+    else
+        wdiff <(cat - <<<"$output") "$expected"
+        newline
+        ((err++))
+    fi
+done
+
+exit $err
